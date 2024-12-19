49cad00f6e93209c420fd978cf7c225ece8093d5
mikiprooon <ryuyakaji0505@gmail.com>
2024-12-19 17:06:13 +0900

    Initial Commit

---
+++ MixamoBlend/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MixamoBlend
+//
+//  Created by 加地竜也 on 2024/12/19.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
